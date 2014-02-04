3384c7c4fbfcb9faf7081276b0f96943426c71af
Grégoire Audoux <projects@gregoire-audoux.fr>
2014-02-04 18:10:45 +0100

    Bump version from 0.0.0 to 0.1.0

--- app/scripts/services/Version.js
+++ app/scripts/services/Version.js
@@ -4,6 +4,6 @@ angular.module('SunnyPass.Webapp')
     .factory('Version', function VersionFactory() {
         return {
             type: 'alpha',
-            version: 'v0.0.2'
+            version: 'v0.1.0'
         };
     });
