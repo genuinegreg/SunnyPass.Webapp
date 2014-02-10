72e24045ecf4b46bddb6fa9f01f097b063a48200
Grégoire Audoux <projects@gregoire-audoux.fr>
2014-02-10 16:33:45 +0100

    Fix a broken dependency on bootstrap  from SunnPass to SunnyPass

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,7 +19,7 @@ angular.module('SunnyPass.Webapp', [
                 }];
             },
             lockers: function () {
-                return ['SunnPass', function (SunnyPass) {
+                return ['SunnyPass', function (SunnyPass) {
                     return SunnyPass.list();
                 }];
             }
@@ -30,58 +30,58 @@ angular.module('SunnyPass.Webapp', [
 
 
         $stateProvider
-            .state('dashboard', {
-                url: '/',
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl',
+            .state('root', {
                 abstract: true,
-                resolve: {
-                    lockers: resolve.lockers()
-                }
-            })
-            .state('more', {
-                url: '/more',
-                templateUrl: 'views/more.html',
-                controller: 'MoreCtrl',
+                templateUrl: 'views/root.html',
+                controller: function($scope, $state) {
+                    $scope.$state = $state;
+                },
                 resolve: {
                     lockers: resolve.lockers()
                 }
             })
 
-            .state('create', {
+            .state('root.dashboard', {
+                url: '/',
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .state('root.more', {
+                url: '/more',
+                templateUrl: 'views/more.html',
+                controller: 'MoreCtrl'
+            })
+
+            .state('root.create', {
                 url: '/create',
                 templateUrl: 'views/locker/create.html',
-                controller: 'LockerCreateCtrl',
-                resolve: {
-                    lockers: resolve.lockers()
-                }
+                controller: 'LockerCreateCtrl'
             })
-            .state('locker', {
+            .state('root.locker', {
                 url: '/locker/:sharedSecret',
                 controller: 'LockerCtrl',
                 abstract: true,
                 templateUrl: 'views/locker.html',
                 resolve: {
-                    locker: resolve.locker(),
-                    lockers: resolve.lockers()
+                    locker: resolve.locker()
                 }
             })
-            .state('locker.content', {
-                url: '/',
+            .state('root.locker.content', {
+                url: '',
                 templateUrl: 'views/locker/content.html',
                 controller: 'LockerContentCtrl'
             })
-            .state('locker.add', {
+            .state('root.locker.add', {
                 url: '/add',
                 templateUrl: 'views/locker/add.html',
                 controller: 'LockerAddCtrl'
             })
-            .state('locker.details', {
+            .state('root.locker.details', {
                 url: '/details',
                 templateUrl: 'views/locker/details.html',
                 controller: 'LockerDetailsCtrl'
             })
-            .state('locker.item', {
+            .state('root.locker.content.item', {
                 url: '/:itemId',
                 templateUrl: 'views/locker/item.html',
                 controller: 'LockerItemCtrl'
